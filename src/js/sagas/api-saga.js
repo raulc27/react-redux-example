4cb339000aa091b0efa5deac50b4e9bf42f4d4a6
Raul Castro <raulc27@gmail.com>
2020-04-29 12:39:56 -0300

    alterando saga worker (url)

--- src/js/sagas/api-saga.js
+++ src/js/sagas/api-saga.js
@@ -6,7 +6,7 @@ export default function* watcherSaga(){
 
 function* workerSaga(){
     try{
-        const payload = yield call(getData);
+        const payload = yield call(getData, action.payload.url);
         yield put({type:"DATA_LOADED",payload});
     }catch(e){
         yield put({type:"API_ERRORED",payload: e});
@@ -14,8 +14,20 @@ function* workerSaga(){
 
 }
 
+
+
+    function getData(url){
+        return fetch(url).then(response=>response.json());
+    }
+
+/*
 function getData(){
     return fetch("https://jsonplaceholder.typicode.com/posts").then(response=>
     response.json()
     );
+    */
+
+
+
+
 }
